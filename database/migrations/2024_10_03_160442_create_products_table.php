63e908294811804a453e827a0b0fc2b0106948d4
Denason <mohammad.wartan@gmail.com>
2024-11-07 18:28:13 +0330

    shop sidebar

--- database/migrations/2024_10_03_160442_create_products_table.php
+++ database/migrations/2024_10_03_160442_create_products_table.php
@@ -13,7 +13,21 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+           // $table->foreignId('brand_id')->constrained()->onDelete('cascade');
+            $table->char('sku', 50)->unique(); // Assuming SKU has a max length of 50
+            $table->string('name');
+            $table->string('details')->nullable();
+            $table->text('description')->nullable();
+            $table->boolean('is_featured')->default(false);
+            $table->boolean('is_active')->default(true);
+            $table->date('stop_selling')->nullable();
+            $table->unsignedBigInteger('views')->default(0);
+            $table->string('unit');
+            $table->decimal('discount', 5, 2)->default(0.00);
             $table->timestamps();
+            // Adding indexes for performance improvements
+            $table->index('name'); // If name is frequently queried
+            $table->index('views'); // If views are frequently queried
         });
     }
 
--- resources/views/livewire/admin/template/multi-sidebar.blade.php
+++ resources/views/livewire/admin/template/multi-sidebar.blade.php
@@ -1,6 +1,21 @@
 <x-menu activate-by-route>
     <x-menu-item title="داشبورد" icon="o-home" link="{{ route('master.dashboard') }}"/>
 
+
+    <x-menu-sub title="فروشگاه" icon="o-shopping-bag">
+
+        <x-menu-item title="محصولات" icon="o-archive-box-arrow-down" link=""/>
+        <x-menu-item title="سفارش‌ها" icon="o-queue-list" link=""/>
+
+        <x-menu-item title="دسته بندی" icon="o-rectangle-group" link=""/>
+        <x-menu-item title="تامین کنندگان" icon="o-user-group" link=""/>
+        <x-menu-item title="کوپن ها" icon="o-gift" link=""/>
+
+
+
+    </x-menu-sub>
+
+
     <x-menu-sub title="بلاگ" icon="o-pencil-square">
 
         <x-menu-item title="ایجاد" icon="o-pencil" link="{{ route('master.blog.create') }}"/>
@@ -8,6 +23,9 @@
         <x-menu-item title="دسته" icon="o-book-open" link="{{ route('master.blog.categories') }}"/>
 
     </x-menu-sub>
+
+
+
 
 
     <x-menu-separator></x-menu-separator>
