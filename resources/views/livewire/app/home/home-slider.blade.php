c7e3aa123367b914eb82dfdc5623d8fe02adb51f
Denason <mohammad.wartan@gmail.com>
2024-11-21 12:35:05 +0330

    footer start

--- resources/views/livewire/app/home/home-slider.blade.php
+++ resources/views/livewire/app/home/home-slider.blade.php
@@ -2,24 +2,24 @@
     $slide1 = asset('storage/photos/slider/slider1.jpg');
     $slides = [
         [
-            'image' => $slide1 ,
+            'image' => 'https://picsum.photos/500/200' ,
             'title' => 'سوغات کوهسار دنا',
             'description' => 'محصولات ارگانیک و تازه، با طعمی اصیل از دل دنا. تجربه خریدی سالم و طبیعی',
             'url' => '/docs/installation',
             'urlText' => 'مشاهده محصولات',
         ],
         [
-            'image' => 'https://img.daisyui.com/images/stock/photo-1609621838510-5ad474b7d25d.webp' ,
+            'image' => 'https://picsum.photos/500/300' ,
             'title' => 'Full stack developers',
             'description' => 'Where burnout is just a fancy term for Tuesday.',
         ],
         [
-            'image' => "https://img.daisyui.com/images/stock/photo-1414694762283-acccc27bca85.webp" ,
+            'image' => "https://picsum.photos/500/400" ,
             'url' => '/docs/installation',
             'urlText' => 'Let`s go!',
         ],
         [
-            'image' => "https://img.daisyui.com/images/stock/photo-1665553365602-b2fb8e5d1707.webp" ,
+            'image' => "https://picsum.photos/400/200" ,
             'url' => '/docs/installation',
         ],
     ];
