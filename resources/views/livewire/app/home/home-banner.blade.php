5830927e615403bad528536e995ad028bfaf4865
Denason <mohammad.wartan@gmail.com>
2024-11-21 12:43:15 +0330

    footer start

--- resources/views/livewire/app/home/home-banner.blade.php
+++ resources/views/livewire/app/home/home-banner.blade.php
@@ -2,7 +2,7 @@
     <div class=" card bg-base-100 image-full w-full shadow-xl ms-2 h-64 ">
     <figure class="">
         <img
-            src="https://picsum.photos/500/200"
+            src="https://picsum.photos/500/800"
             alt="Product Banner" />
     </figure>
     <div class="card-body">
