39430a84390e5649e978c1e97b0916e9eecfe788
Denason <mohammad.wartan@gmail.com>
2024-11-26 02:28:08 +0330

    repair pics

--- app/Livewire/App/Shop/SingleInc/ProductGallery.php
+++ app/Livewire/App/Shop/SingleInc/ProductGallery.php
@@ -1,25 +1,25 @@
 <?php
 
 namespace App\Livewire\App\Shop\SingleInc;
+
 use App\Models\Product;
 use Illuminate\Support\Facades\Cache;
 use Livewire\Attributes\Layout;
-use Livewire\Attributes\Lazy;
 use Livewire\Component;
 use Mary\Traits\Toast;
-#[Layout('components.layouts.app')]
 
+#[Layout('components.layouts.app')]
 class ProductGallery extends Component
 {
-use Toast;
+    use Toast;
+
     public $images;
     public Product $product;
 
     public function mount()
     {
 
-        $this->images = Cache::remember('slider-images-'.$this->product->id, now()->addMinutes(30), function ()
-        {
+        $this->images = Cache::remember('slider-images-' . $this->product->id, now()->addMinutes(30), function () {
             return $this->product->images->pluck('file_path')->map(function ($filePath) {
 
                 return asset($filePath);
