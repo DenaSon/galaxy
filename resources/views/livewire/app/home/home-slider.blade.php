3060a458e44e6e4b792ad5a038f650ad9d96b712
Denason <mohammad.wartan@gmail.com>
2024-11-27 13:28:30 +0330

    add slider 1

--- resources/views/livewire/app/home/home-slider.blade.php
+++ resources/views/livewire/app/home/home-slider.blade.php
@@ -1,14 +1,25 @@
 @php
     $slide1 = asset('static/slider/slider-nuts.JPG');
+    $slide2 = asset('static/slider/slider2.jpg');
     $slides = [
-        [
-            'image' => $slide1 ,
-            'title' => 'خشکبار خوشمزه',
-            'description' => 'محصولات ارگانیک و تازه، ',
-            'url' => singleCategoryUrl(1,'خشکبار'),
+ [
+    'image' => $slide1,
+    'title' => 'تنقلات',
+    'description' => 'خوشمزه‌ترین و سالم‌ترین تنقلات برای لحظات شاد شما. از مغزها تا چیپس‌های خانگی!',
+    'url' => singleCategoryUrl(1,'تنقلات'),
+    'urlText' => 'مشاهده محصولات',
+]
+,
 
-            'urlText' => 'مشاهده محصولات',
-        ],
+  [
+    'image' => $slide2,
+    'title' => 'خشکبار خوشمزه',
+    'description' => 'محصولات ارگانیک و تازه، سرشار از طعم و خواص عالی برای سلامت شما!',
+    'url' => singleCategoryUrl(1,'خشکبار'),
+    'urlText' => 'مشاهده محصولات',
+]
+
+
     ];
 @endphp
 
