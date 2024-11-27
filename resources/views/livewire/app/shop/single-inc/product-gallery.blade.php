c9ee49c07da55ddf037acb3045952b1eb5d2cf74
Denason <mohammad.wartan@gmail.com>
2024-11-27 14:41:13 +0330

    add slider 1

--- resources/views/livewire/app/shop/single-inc/product-gallery.blade.php
+++ resources/views/livewire/app/shop/single-inc/product-gallery.blade.php
@@ -13,7 +13,7 @@
     </div>
 
 
-    <x-image-gallery :images="$images" class="h-40 rounded-box w-80" with-arrows="true" with-indicators="true"/>
+    <x-image-gallery :images="$images" class="h-28 rounded-box w-40" with-arrows with-indicators/>
 
 
 </div>
