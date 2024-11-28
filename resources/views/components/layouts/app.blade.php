7379e22ff972bcd2389b6973be40f979bc17516f
Denason <mohammad.wartan@gmail.com>
2024-11-28 16:27:25 +0330

    change scrollbar style

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -17,6 +17,37 @@
             font-weight: normal;
             font-style: normal;
         }
+
+        ::-webkit-scrollbar {
+            width: 12px;
+        }
+
+        ::-webkit-scrollbar-thumb {
+            background-color: #888;
+            border-radius: 10px;
+        }
+
+        ::-webkit-scrollbar-thumb:hover {
+            background-color: #555;
+        }
+
+        ::-webkit-scrollbar-track {
+            background: #f1f1f1;
+        }
+
+
+        * {
+            scrollbar-width: thin;
+            scrollbar-color: #888 #f1f1f1;
+        }
+
+
+        body {
+            -ms-overflow-style: scrollbar;
+        }
+
+
+
     </style>
 
 
