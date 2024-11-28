4f4b7e1b40d59e9b80441a8cdf1b70cc68e99b73
Denason <mohammad.wartan@gmail.com>
2024-11-28 16:24:26 +0330

    add seo details for category list

--- resources/views/livewire/app/shop/category-inc/seo.blade.php
+++ resources/views/livewire/app/shop/category-inc/seo.blade.php
@@ -3,6 +3,31 @@
 <link rel="canonical" href="{{singleCategoryUrl($category->id,$category->name)}}">
 <meta property="og:type" content="website">
 <meta property="og:title" content="{{ $category->name }} - DenaPax">
-<meta property="og:description" content="مشاهده لیست محصولات دسته‌بندی شده در سایت DenaPax. انتخاب {{ $category->name }} با بهترین کیفیت.">
+<meta property="og:description"
+      content="مشاهده لیست محصولات دسته‌بندی شده در سایت DenaPax. انتخاب {{ $category->name }} با بهترین کیفیت.">
 <meta property="og:url" content="{{singleCategoryUrl($category->id,$category->name)}}">
 <meta property="og:site_name" content="{{ getSetting('website_title') }}">
+
+<script type="application/ld+json">
+    {
+      "@context": "https://schema.org",
+      "@type": "ItemList",
+      "name": "{{ $category->name }}",
+  "url": "{{ singleCategoryUrl($category->id, $category->name) }}",
+  "itemListElement": [
+    @foreach ($category->products as $index => $product)
+        {
+          "@type": "ListItem",
+          "position": {{ $index + 1 }},
+        "url": "{{ singleProductUrl($product->id,$product->name) }}",
+        "name": "{{ $product->name }}",
+        "image": "{{ asset($product->images()->first()->file_path ?? '') ?? '' }}",
+        "price": "{{ number_format($product->variants->min('price'))  }}",
+        "priceCurrency": "IRT"
+      } @if (!$loop->last)
+            ,
+        @endif
+    @endforeach
+    ]
+  }
+</script>
