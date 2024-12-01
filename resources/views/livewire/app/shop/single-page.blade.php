1a04f7fc36cdd2762b2265feb34e855c6f74acb2
Denason <mohammad.wartan@gmail.com>
2024-12-01 19:41:59 +0330

    blog card

--- resources/views/livewire/app/shop/single-page.blade.php
+++ resources/views/livewire/app/shop/single-page.blade.php
@@ -4,17 +4,7 @@
     @endpush
     <article class="container mx-auto lg:px-2 2xl:px-0 mt-2">
 
-        <div class="w-full border rounded-box p-2 pt-3">
-
-
-
-
-        </div>
-
-
         <div class="flex flex-col lg:flex-row gap-4 mt-4">
-
-
 
             <div class="w-full lg:w-5/6 h-auto  mt-5">
                 <h1 class="text-center font-black text-2xl mb-3"> {{ $page->title }}</h1>
@@ -22,7 +12,6 @@
                 <div class="!text-sm !font-normal !leading-9 text-justify mb-5">
                     {!! $page->content !!}
                 </div>
-
 
             </div>
 
