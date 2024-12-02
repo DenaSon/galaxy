ba81643928a512cbf76f1cb2234ff570b6db5138
Denason <mohammad.wartan@gmail.com>
2024-12-02 12:01:58 +0330

    artisan send log file

---
+++ app/Console/Commands/SendLogEmail.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\File;
+use Illuminate\Support\Facades\Mail;
+
+class SendLogEmail extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:send-log-email';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+
+        $logFile = storage_path('logs/laravel.log');
+
+
+        if (File::exists($logFile)) {
+
+            $logContent = File::get($logFile);
+
+
+            Mail::raw($logContent, function ($message) {
+                $message->to(getSetting('admin_email') ?? 'info@denapax.com')
+                    ->subject('Laravel Log File');
+            });
+
+            $this->info('Log file has been sent via email.');
+        } else {
+            $this->error('Log file does not exist.');
+        }
+    }
+}
