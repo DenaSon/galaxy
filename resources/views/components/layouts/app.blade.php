0a399fb358d9779c647258eaa46d801c2ff7cf66
Denason <mohammad.wartan@gmail.com>
2024-12-07 09:18:01 +0330

    add microsoft clarity

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -9,7 +9,6 @@
     @stack('SEO')
     @stack('styles')
 
-
     <style>
         @font-face {
             font-family: 'denapax-font';
@@ -65,7 +64,13 @@
 
     <meta name="csrf-token" content="{{ csrf_token() }}">
     @vite(['resources/css/app.css', 'resources/js/app.js'])
-
+    <script type="text/javascript">
+        (function(c,l,a,r,i,t,y){
+            c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+            t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+            y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+        })(window, document, "clarity", "script", "pa3r8en4m1");
+    </script>
 
 </head>
 
