ade3e95d38f2073f9553f354580e4c31d742b181
Denason <mohammad.wartan@gmail.com>
2024-12-07 11:45:05 +0330

    add users component

--- app/Livewire/Admin/Component/UserList.php
+++ app/Livewire/Admin/Component/UserList.php
@@ -27,17 +27,23 @@ class UserList extends Component
 
     public function render()
     {
-        $users = User::withCount('orders')
-            ->withMax('orders', 'created_at')
-            ->withSum('orders', 'grand_total')
+        $users = User::withCount(['orders' => function ($query) {
+            $query->where('payment_status', 'paid'); // فقط سفارش‌های پرداخت‌شده
+        }])
+            ->withMax(['orders' => function ($query) {
+                $query->where('payment_status', 'paid'); // آخرین تاریخ سفارش‌های پرداخت‌شده
+            }], 'created_at')
+            ->withSum(['orders' => function ($query) {
+                $query->where('payment_status', 'paid'); // مجموع مبلغ سفارش‌های پرداخت‌شده
+            }], 'grand_total')
             ->with(['orders' => function ($query) {
-                $query->withSum('orderItems', 'quantity');
-
+                $query->where('payment_status', 'paid') // فقط سفارش‌های پرداخت‌شده
+                ->withSum('orderItems', 'quantity'); // مجموع تعداد آیتم‌های هر سفارش
             }])
-
             ->orderBy('orders_count', $this->sortBy)
             ->latest()
             ->get();
+
 
 
         return view('livewire.admin.component.user-list', compact('users'));
