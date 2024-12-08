40d80cd5aed77cdb0962c9ad5f5ddf63a3ce372c
Denason <mohammad.wartan@gmail.com>
2024-12-08 21:01:44 +0330

    add swiper slider-builder

--- resources/views/livewire/app/shop/single-inc/product-gallery.blade.php
+++ resources/views/livewire/app/shop/single-inc/product-gallery.blade.php
@@ -1,37 +1,42 @@
 <div class="w-full lg:w-2/6 text-center">
-<section class="container mx-auto" style="z-index: 0 !important;">
+        @push('styles')
+            <script src="https://cdn.jsdelivr.net/npm/photoswipe@5.4.3/dist/umd/photoswipe.umd.min.js"></script>
+            <script src="https://cdn.jsdelivr.net/npm/photoswipe@5.4.3/dist/umd/photoswipe-lightbox.umd.min.js"></script>
+            <link href="https://cdn.jsdelivr.net/npm/photoswipe@5.4.3/dist/photoswipe.min.css" rel="stylesheet">
+        @endpush
+    <section class="container mx-auto" style="z-index: 0 !important;">
 
-    <div class="swiper m-3">
+        <div class="swiper m-3">
 
 
-        <div class="swiper-wrapper">
+            <div class="swiper-wrapper">
 
-                    @foreach($images as $image)
-                        <div class="swiper-slide">
-                            <img src="{{ asset($image) }}" alt="{{ $product->name }}" height="480"/>
-                        </div>
-                    @endforeach
+                        @foreach($images as $image)
+                            <div class="swiper-slide">
+                                <img src="{{ asset($image) }}" alt="{{ $product->name }}" height="480"/>
+                            </div>
+                        @endforeach
+
+            </div>
+
+            <div class="swiper-pagination"></div>
+
+
+            <div class="swiper-button-prev"></div>
+            <div class="swiper-button-next"></div>
 
         </div>
 
-        <div class="swiper-pagination"></div>
-
-
-        <div class="swiper-button-prev"></div>
-        <div class="swiper-button-next"></div>
-
+    </section>
+        <x-image-gallery class="mt-3" :images="$images" class="h-20 rounded-box w-70" with-arrows with-indicators/>
     </div>
 
-</section>
-    <x-image-gallery class="mt-3" :images="$images" class="h-20 rounded-box w-70" with-arrows with-indicators/>
-</div>
 
 
 
 
 
-
-{{--<div class="w-full lg:w-2/6 text-center">--}}
+    {{--<div class="w-full lg:w-2/6 text-center">--}}
 {{--    @push('styles')--}}
 {{--        <script src="https://cdn.jsdelivr.net/npm/photoswipe@5.4.3/dist/umd/photoswipe.umd.min.js"></script>--}}
 {{--        <script src="https://cdn.jsdelivr.net/npm/photoswipe@5.4.3/dist/umd/photoswipe-lightbox.umd.min.js"></script>--}}
