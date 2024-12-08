e35e5c666d369c10e439f99c94d87f26246654b9
Denason <mohammad.wartan@gmail.com>
2024-12-08 23:08:10 +0330

    flip effect add

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,36 +2,8 @@ import "./libs/trix";
 import './bootstrap';
 import '../css/app.css';
 import '../css/style.css';
-
 import Swiper from 'swiper/bundle';
-
 // import styles bundle
 import 'swiper/css/bundle';
 
-
-function startSwiper() {
-    // init Swiper:
-    const swiper = new Swiper('.swiper', {
-
-        effect: 'cube',
-        cubeEffect: {
-            shadow: true,
-            slideShadows: true,
-        },
-
-        navigation: {
-            nextEl: '.swiper-button-next',
-            prevEl: '.swiper-button-prev',
-        },
-
-    });
-
-}
-
-startSwiper();
-
-document.addEventListener('livewire:navigated', (event) => {
-
-    startSwiper();
-
-},{ once: false });
+import '../js/libs/swiper.js'
---
+++ resources/js/libs/swiper.js
@@ -0,0 +1,26 @@
+function startSwiper() {
+    // init Swiper:
+    const swiper = new Swiper('.swiper', {
+
+        effect: 'flip',
+        // cubeEffect: {
+        //     shadow: true,
+        //     slideShadows: true,
+        // },
+
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+
+    });
+
+}
+
+startSwiper();
+
+document.addEventListener('livewire:navigated', (event) => {
+
+    startSwiper();
+
+}, {once: false});
