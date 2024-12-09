c368bcab31e06b481770ce933b3ec104d5178972
Denason <mohammad.wartan@gmail.com>
2024-12-09 16:19:07 +0330

    add goftino

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -3,9 +3,7 @@
 <html lang="fa">
 <head>
     @include('components.layouts.inc.analytics-code')
-    <script type="text/javascript">
-        !function(){var i="qcNyHg",a=window,d=document;function g(){var g=d.createElement("script"),s="https://www.goftino.com/widget/"+i,l=localStorage.getItem("goftino_"+i);g.async=!0,g.src=l?s+"?o="+l:s;d.getElementsByTagName("head")[0].appendChild(g);}"complete"===d.readyState?g():a.attachEvent?a.attachEvent("onload",g):a.addEventListener("load",g,!1);}();
-    </script>
+
     <link rel="preload" href="{{asset('admin/assets/fonts/iransans/woff2/IRANSansWeb(FaNum).woff2')}}" as="font"
           type="font/woff2" crossorigin="anonymous">
     <meta charset="UTF-8">
