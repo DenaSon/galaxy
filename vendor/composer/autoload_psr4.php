162f31213d56e7d3f6baa2e68473323deb767e79
Denason <mohammad.wartan@gmail.com>
2024-12-11 22:45:02 +0330

    add alert to add to xart

--- vendor/composer/autoload_psr4.php
+++ vendor/composer/autoload_psr4.php
@@ -73,7 +73,7 @@ return array(
     'Mary\\' => array($vendorDir . '/robsontenorio/mary/src'),
     'Livewire\\Volt\\' => array($vendorDir . '/livewire/volt/src'),
     'Livewire\\' => array($vendorDir . '/livewire/livewire/src'),
-    'League\\Uri\\' => array($vendorDir . '/league/uri-interfaces', $vendorDir . '/league/uri'),
+    'League\\Uri\\' => array($vendorDir . '/league/uri', $vendorDir . '/league/uri-interfaces'),
     'League\\MimeTypeDetection\\' => array($vendorDir . '/league/mime-type-detection/src'),
     'League\\Flysystem\\Local\\' => array($vendorDir . '/league/flysystem-local'),
     'League\\Flysystem\\' => array($vendorDir . '/league/flysystem/src'),
