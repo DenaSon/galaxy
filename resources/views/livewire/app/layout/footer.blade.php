e1598f7cdbbccc89ed4bfdec79a11268bdd79cc5
Denason <mohammad.wartan@gmail.com>
2024-12-19 16:53:55 +0330

    update cart ico in app

--- resources/views/livewire/app/layout/footer.blade.php
+++ resources/views/livewire/app/layout/footer.blade.php
@@ -94,7 +94,7 @@ hidden md:block
 
 
         <aside>
-            <x-icon name="o-sparkles" class="h-20 w-20 text-primary"></x-icon>
+            <x-icon name="o-sparkles" class="h-25 w-20 text-primary"></x-icon>
 
             <p>
                 دنا پکس | از دنا به خانه شما، جایی که طبیعت و اصالت به هم می‌رسند!
