2200856398c33098a3d9dfbfac6e54fdec7be185
Denason <mohammad.wartan@gmail.com>
2025-01-04 11:17:07 +0330

    single blog improvement-related

--- resources/views/livewire/app/shop/related-blog.blade.php
+++ resources/views/livewire/app/shop/related-blog.blade.php
@@ -2,18 +2,12 @@
     @if (!empty($blogContent) && isset($blogContent['content']))
         <div class="mt-4 single-blog">
 
-                <?php
-                $contentWithoutImages = preg_replace('/<img[^>]*>/i', '', $blogContent['content']['rendered']);
-                $limitedContent = \Illuminate\Support\Str::limit($contentWithoutImages, 2560, '...');
-                ?>
-
-                 {!! $limitedContent !!}
+            {!! \Illuminate\Support\Str::limit(preg_replace('/<img[^>]*>/i', '', $blogContent['content']['rendered']), 2500, '...') !!}
 
 
             <div class="flex justify-center">
                 <x-button
                     external
-                    class="btn-block"
                     icon="o-ellipsis-horizontal-circle"
                     link="{{ singleBlogUrl($blogContent['id'],$blogContent['title']['rendered']) }}"
                     label="ادامه مطلب"
