d38f758d35bc5aa7834f9aef9b138db037b305e6
Denason <mohammad.wartan@gmail.com>
2025-01-06 11:15:05 +0330

    add supplier registration

--- resources/views/livewire/app/supplier/local/local-register.blade.php
+++ resources/views/livewire/app/supplier/local/local-register.blade.php
@@ -1,28 +1,23 @@
-<div>
+<div class="flex justify-center items-center">
 
     <div class="container mx-auto">
+        <x-card class="flex flex-col items-center  max-w-4xl" separator title="ثبت‌نام تامین کنندگان">
 
-
-        <x-card class="flex justify-center items-center" separator title="ثبت‌نام تامین کنندگان">
-
-
-            <x-steps wire:model="step" class="border my-5 p-5">
-                <x-step step="1" text="ثبت‌نام">
+            <x-steps wire:model="step" class="border my-5 p-5 w-3/4 max-w-2xl">
+                <x-step step="1" text="ثبت‌نام" step-classes="w-full">
                     @if(Auth::check())
                         <div class="text-center">
                             <b> {{ Auth::user()->phone }} </b>
-
                         </div>
                     @else
                         <x-button wire:click="loginAction" label="ثبت‌نام" class="btn btn-primary btn-block"/>
                     @endif
                 </x-step>
-                <x-step step="2" text="اطلاعات">
+                <x-step step="2" text="ثبت اطلاعات">
                     <x-form>
                         <x-input inline="" placeholder="نام" wire:model.live="name"/>
                         <x-input placeholder="نام خانوادگی" wire:model.live="last_name"/>
                         <x-textarea inline="" placeholder="محصولات قابل تامین"/>
-
                     </x-form>
                 </x-step>
                 <x-step step="3" text="تایید نهایی" class="bg-orange-500/20">
@@ -30,16 +25,13 @@
                 </x-step>
             </x-steps>
 
-            {{-- Create some methods to increase/decrease the model to match the step number --}}
-            {{-- You could use Alpine with `$wire` here --}}
-            <x-button label="مرحله قبل" wire:click="prev"/>
-            <x-button class="btn-success" label="مرحله بعد" wire:click="next"/>
-
+            {{-- دکمه‌های ناوبری مراحل --}}
+            <div class="flex justify-between w-3/4 max-w-2xl mt-5">
+                <x-button label="مرحله قبل" wire:click="prev" class="btn-outline"/>
+                <x-button class="btn-success" label="مرحله بعد" wire:click="next"/>
+            </div>
 
         </x-card>
-
-
     </div>
-
 
 </div>
