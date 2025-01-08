4259d3d9086d04c3d8925bac21c50be6e83fe985
Denason <mohammad.wartan@gmail.com>
2025-01-08 18:53:21 +0330

    connect to wikipedia

--- app/Support/global.php
+++ app/Support/global.php
@@ -414,7 +414,7 @@ function getWikipediaInfo($title=''): string
             'action' => 'query',
             'format' => 'json',
             'titles' => $title,
-            'prop' => 'extracts',
+            'prop' => 'text',
             'exintro' => true,
         ]);
 
