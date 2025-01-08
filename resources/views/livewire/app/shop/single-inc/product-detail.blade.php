22b6637e413626d73baae20c745e854b2f2203b7
Denason <mohammad.wartan@gmail.com>
2025-01-08 19:19:41 +0330

    connect to wikipedia

--- resources/views/livewire/app/shop/single-inc/product-detail.blade.php
+++ resources/views/livewire/app/shop/single-inc/product-detail.blade.php
@@ -101,7 +101,7 @@
                           </p>
                        </div>
                      <br/>
-                    <span class="font-thin text-xs text-gray-400 mt-2 badge badge-warning">
+                    <span class="font-thin text-xs text-gray-50 mt-2 badge badge-warning">
                      استخراج شده از :  <a href="https://fa.wikipedia.org/" target="_blank" rel="noopener">ویکی پدیا </a>
 
                     </span>
