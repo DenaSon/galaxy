eab458965645b61106cd8602d64c288b0b85c8df
Denason <mohammad.wartan@gmail.com>
2025-02-02 14:56:46 +0330

    fix fonts-run build npm

--- resources/views/components/layouts/inc/analytics-code.blade.php
+++ resources/views/components/layouts/inc/analytics-code.blade.php
@@ -1,22 +1,22 @@
-<!-- Google tag (gtag.js) -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-L9BG3H72V6"></script>
-<script>
-    window.dataLayer = window.dataLayer || [];
-    function gtag(){dataLayer.push(arguments);}
-    gtag('js', new Date());
+{{--<!-- Google tag (gtag.js) -->--}}
+{{--<script async src="https://www.googletagmanager.com/gtag/js?id=G-L9BG3H72V6"></script>--}}
+{{--<script>--}}
+{{--    window.dataLayer = window.dataLayer || [];--}}
+{{--    function gtag(){dataLayer.push(arguments);}--}}
+{{--    gtag('js', new Date());--}}
 
-    gtag('config', 'G-L9BG3H72V6');
-</script>
+{{--    gtag('config', 'G-L9BG3H72V6');--}}
+{{--</script>--}}
 
-<script type="text/javascript">
-    (function (c, l, a, r, i, t, y) {
-        c[a] = c[a] || function () {
-            (c[a].q = c[a].q || []).push(arguments)
-        };
-        t = l.createElement(r);
-        t.async = 1;
-        t.src = "https://www.clarity.ms/tag/" + i;
-        y = l.getElementsByTagName(r)[0];
-        y.parentNode.insertBefore(t, y);
-    })(window, document, "clarity", "script", "pa3r8en4m1");
-</script>
+{{--<script type="text/javascript">--}}
+{{--    (function (c, l, a, r, i, t, y) {--}}
+{{--        c[a] = c[a] || function () {--}}
+{{--            (c[a].q = c[a].q || []).push(arguments)--}}
+{{--        };--}}
+{{--        t = l.createElement(r);--}}
+{{--        t.async = 1;--}}
+{{--        t.src = "https://www.clarity.ms/tag/" + i;--}}
+{{--        y = l.getElementsByTagName(r)[0];--}}
+{{--        y.parentNode.insertBefore(t, y);--}}
+{{--    })(window, document, "clarity", "script", "pa3r8en4m1");--}}
+{{--</script>--}}
