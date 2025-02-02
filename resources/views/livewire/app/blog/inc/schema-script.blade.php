76a9b432ecac31b2693e86e55e18018a4f263509
Denason <mohammad.wartan@gmail.com>
2025-02-02 11:35:25 +0330

    fix bug in blog index

--- resources/views/livewire/app/blog/inc/schema-script.blade.php
+++ resources/views/livewire/app/blog/inc/schema-script.blade.php
@@ -7,7 +7,7 @@
             "@id": "{{ singleBlogUrl($article['id'], $article['title']['rendered']) }}"
     },
     "headline": "{{ e($article['title']['rendered']) }}",
-    "description": "{{ $article['yoast_head_json']['description'] ?? ''  }}",
+    "description": "{{ $article['yoast_head_json']['description'] ?? strip_tags($article['excerpt']['rendered']) }}",
     "image": "{{ $featuredImageUrl ?? '' }}", {{-- Update with the actual featured image --}}
     "author": {
         "@type": "Person",
@@ -26,11 +26,11 @@
 }
 </script>
 
-<meta name="description" content="{{ $article['yoast_head_json']['description'] ?? ''   }}">
+<meta name="description" content="{{ $article['yoast_head_json']['description'] ?? strip_tags($article['excerpt']['rendered']) }}">
 <link rel="canonical" href="{{ singleBlogUrl($article['id'], $article['title']['rendered']) }}">
 <meta property="og:type" content="{{ $article['yoast_head_json']['og_type']  }}">
 <meta property="og:title" content="{{ $article['title']['rendered'] }}">
-<meta property="og:description" content="{{ $article['yoast_head_json']['description'] ?? ''  }}">
+<meta property="og:description" content="{{ $article['yoast_head_json']['description'] ?? strip_tags($article['excerpt']['rendered']) }}">
 <meta property="og:url" content="{{ singleBlogUrl($article['id'], $article['title']['rendered']) }}">
 <meta property="og:image" content="{{ $article['yoast_head_json']['og_image'][0]['url']  }}">
 <meta property="og:site_name" content="{{ getSetting('website_title') }}">
