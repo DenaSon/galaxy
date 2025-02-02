d4d56831f92224fb87db5ffd78ae28391c301f4e
Denason <mohammad.wartan@gmail.com>
2025-02-02 11:26:48 +0330

    fix bug in blog index

--- resources/views/livewire/app/blog/inc/schema-script.blade.php
+++ resources/views/livewire/app/blog/inc/schema-script.blade.php
@@ -26,11 +26,11 @@
 }
 </script>
 
-<meta name="description" content="{{ $article['yoast_head_json']['description']   }}">
+<meta name="description" content="{{ $article['yoast_head_json']['description'] ?? ''   }}">
 <link rel="canonical" href="{{ singleBlogUrl($article['id'], $article['title']['rendered']) }}">
 <meta property="og:type" content="{{ $article['yoast_head_json']['og_type']  }}">
 <meta property="og:title" content="{{ $article['title']['rendered'] }}">
-<meta property="og:description" content="{{ $article['yoast_head_json']['description']  }}">
+<meta property="og:description" content="{{ $article['yoast_head_json']['description'] ?? ''  }}">
 <meta property="og:url" content="{{ singleBlogUrl($article['id'], $article['title']['rendered']) }}">
 <meta property="og:image" content="{{ $article['yoast_head_json']['og_image'][0]['url']  }}">
 <meta property="og:site_name" content="{{ getSetting('website_title') }}">
@@ -39,5 +39,5 @@
 <meta property="article:modified_time" content="{{ \Carbon\Carbon::parse($article['modified'])->format('c') }}">
 <meta name="twitter:card" content="summary_large_image">
 <meta name="twitter:title" content="{{ $article['title']['rendered'] }}">
-<meta name="twitter:description" content="{{ $article['yoast_head_json']['description'] }}">
+<meta name="twitter:description" content="{{ $article['yoast_head_json']['description'] ?? '' }}">
 <meta name="twitter:image" content="{{ $article['yoast_head_json']['og_image'][0]['url'] }}">
