5969ed52c8a39097a1c2071dfedf45eb19d6bda3
Denason <mohammad.wartan@gmail.com>
2025-02-03 12:07:26 +0330

    bug fix for csrf tokens in livewire update

--- config/session.php
+++ config/session.php
@@ -60,7 +60,7 @@ return [
     |
     */
 
-    'files' => storage_path('framework/sessions/new'),
+    'files' => storage_path('framework/sessions'),
 
     /*
     |--------------------------------------------------------------------------
@@ -129,7 +129,7 @@ return [
 
     'cookie' => env(
         'SESSION_COOKIE',
-        Str::slug(env('APP_NAME', 'laravel'), '_').'_session'
+        Str::slug(env('APP_NAME', 'laravel'), '_').'_session_x'
     ),
 
     /*
