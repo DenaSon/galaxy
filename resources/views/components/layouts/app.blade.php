8ab7a1c5815251a7959fdc7774f05ad4fc52ea98
Denason <mohammad.wartan@gmail.com>
2025-02-03 11:25:52 +0330

    fix fonts-run build npm

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -18,7 +18,6 @@
     <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
 
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <title>{{ $title ?? config('app.name') }}</title>
 
