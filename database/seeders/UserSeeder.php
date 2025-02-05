94783cd51a4aefe953be1c96be75f54c549198c2
Denason <mohammad.wartan@gmail.com>
2025-02-05 22:49:24 +0330

    core updates for denapax

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -13,13 +13,13 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
-        User::create([
-            'id' => 1,
-            'name' => 'محمد',
-            'email' => 'mohammad@example.com', // ایمیل باید منحصر به فرد باشد
-            'email_verified_at' => now(), // تاریخ تأیید ایمیل
-            'password' => bcrypt('password'), // رمز عبور (در اینجا می‌توانید رمز عبور دلخواه خود را وارد کنید)
-            'remember_token' => null, // توکن یادآوری (اختیاری)
-        ]);
+//        User::create([
+//            'id' => 1,
+//            'name' => 'محمد',
+//            'email' => 'mohammad@example.com', // ایمیل باید منحصر به فرد باشد
+//            'email_verified_at' => now(), // تاریخ تأیید ایمیل
+//            'password' => bcrypt('password'), // رمز عبور (در اینجا می‌توانید رمز عبور دلخواه خود را وارد کنید)
+//            'remember_token' => null, // توکن یادآوری (اختیاری)
+//        ]);
     }
 }
