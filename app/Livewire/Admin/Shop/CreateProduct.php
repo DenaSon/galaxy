048350e715a6c5122dc3ab4815dd47e392f128ec
Denason <mohammad.wartan@gmail.com>
2025-02-06 14:23:39 +0330

    update footer links

--- app/Livewire/Admin/Shop/CreateProduct.php
+++ app/Livewire/Admin/Shop/CreateProduct.php
@@ -142,6 +142,7 @@ class CreateProduct extends Component
             'name' => 'required|string|max:255',
             'discount' => 'numeric|nullable|max:100|min:0',
             'selectedCategories' => 'required|array',
+            'related_article_id' =>'numeric|min:0',
 
         ]);
 
