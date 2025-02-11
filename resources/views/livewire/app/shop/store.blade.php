99fbedccecebbbd9e3656b43cce8b62b539acefc
Denason <mohammad.wartan@gmail.com>
2025-02-11 22:37:15 +0330

    update npm and style

--- resources/views/livewire/app/shop/store.blade.php
+++ resources/views/livewire/app/shop/store.blade.php
@@ -37,12 +37,12 @@
 
     <x-progress class="progress-primary h-0.5" indeterminate wire:loading/>
 
-
+    <h1> لیست محصولات  {{ getSetting('website_title') }} </h1>
 
     <div class="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-6 gap-4 mt-4">
 
 
-        <h1> لیست محصولات  {{ getSetting('website_title') }} </h1>
+
 
     @foreach ($products as $product)
             @livewire('app.component.product-card',['product' => $product],key($product->id))
