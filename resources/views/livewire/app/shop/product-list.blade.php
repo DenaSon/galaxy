cbe9767bae57a8d847b4bbf92ea725a8ba5b9912
Denason <mohammad.wartan@gmail.com>
2025-02-12 11:32:05 +0330

    category add mockup

--- resources/views/livewire/app/shop/product-list.blade.php
+++ resources/views/livewire/app/shop/product-list.blade.php
@@ -5,6 +5,18 @@
     @include('livewire.app.shop.category-inc.top-menu')
 
 
+        <section  class="mockup-browser border-base-300 border mt-2 mb-2">
+            <div class="mockup-browser-toolbar text-right">
+                <h1 class="justify-start text-right mx-auto">
+                    خرید    {{ $category->name }}
+                </h1>
+            </div>
+            <p class="border-base-300 flex text-justify justify-start border-t px-4 py-4 leading-relaxed">
+                {{ $category->description }}
+            </p>
+        </section>
+
+
 
 
     <section class="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-6 gap-4 mt-4">
@@ -14,16 +26,7 @@
     </section>
 
 
-        <section  class="mockup-browser border-base-300 border mt-4">
-            <div class="mockup-browser-toolbar text-right">
-                <h1 class="justify-start text-right mx-auto">
-                 خرید    {{ $category->name }}
-                </h1>
-            </div>
-            <div class="border-base-300 flex text-justify justify-start border-t px-4 py-4 leading-relaxed">
-                {{ $category->description }}
-            </div>
-        </section>
+
 
 
 
