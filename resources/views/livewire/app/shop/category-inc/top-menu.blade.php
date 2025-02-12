ccac8cd8fc263d363b594923c762856cc377c0cb
Denason <mohammad.wartan@gmail.com>
2025-02-12 10:41:50 +0330

    category add mockup

--- resources/views/livewire/app/shop/category-inc/top-menu.blade.php
+++ resources/views/livewire/app/shop/category-inc/top-menu.blade.php
@@ -1,12 +1,12 @@
 <div class="w-full flex justify-between items-center">
-    <!-- بخش اول -->
+
     <div class="breadcrumbs text-sm">
         <ul class="text-xs">
             <li><a wire:navigate href="{{ route('home.index-home') }}">صفحه اول</a></li>
             <li><a wire:navigate href="{{ singleCategoryUrl($category->id, $category->name) }}">{{ $category->name }}</a></li>
         </ul>
     </div>
-    <!-- بخش دوم -->
+
     <div class="text-sm text-right">
         <p>
             <x-rating disabled wire:model="ranking" class="bg-warning" total="5" />
--- resources/views/livewire/app/shop/product-list.blade.php
+++ resources/views/livewire/app/shop/product-list.blade.php
@@ -4,11 +4,29 @@
     @endpush
     @include('livewire.app.shop.category-inc.top-menu')
 
+
+
+
     <div class="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-6 gap-4 mt-4">
         @foreach($products as $product)
             @livewire('app.component.product-card', ['product' => $product], key($product->id))
         @endforeach
     </div>
+
+
+        <div class="mockup-browser border-base-300 border mt-4">
+            <div class="mockup-browser-toolbar text-right">
+                <h1 class="justify-start text-right mx-auto">
+                 خرید    {{ $category->name }}
+                </h1>
+            </div>
+            <div class="border-base-300 flex justify-start border-t px-2 py-4  text-pretty">
+                {{ $category->description }}
+            </div>
+        </div>
+
+
+
 
 
     <!-- Centered Pagination -->
