9a22ebe3f4b467a5f90542b75d26c1528bad24a2
Denason <mohammad.wartan@gmail.com>
2025-02-12 11:34:14 +0330

    category add mockup

--- resources/views/livewire/app/shop/product-list.blade.php
+++ resources/views/livewire/app/shop/product-list.blade.php
@@ -5,20 +5,10 @@
     @include('livewire.app.shop.category-inc.top-menu')
 
 
-        <section  class="mockup-browser border-base-300 border mt-2 mb-2">
-            <div class="mockup-browser-toolbar text-right">
-                <h1 class="justify-start text-right mx-auto">
-                    خرید    {{ $category->name }}
-                </h1>
-            </div>
-            <p class="border-base-300 flex text-justify justify-start border-t px-4 py-4 leading-relaxed">
-                {{ $category->description }}
-            </p>
-        </section>
 
 
 
-
+        <main>
     <section class="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-6 gap-4 mt-4">
         @foreach($products as $product)
             @livewire('app.component.product-card', ['product' => $product], key($product->id))
@@ -30,11 +20,21 @@
 
 
 
+        <section  class="mockup-browser border-base-300 border mt-2 mb-2">
+            <header class="mockup-browser-toolbar text-right">
+                <h1 class="justify-start text-right mx-auto">
+                    خرید    {{ $category->name }}
+                </h1>
+            </header>
+            <p class="border-base-300 flex text-justify justify-start border-t px-4 py-4 leading-relaxed text-gray-600">
+                {{ $category->description }}
+            </p>
+        </section>
 
 
     <!-- Centered Pagination -->
     <div class="mt-4 flex justify-center">
         {{ $products->links() }}
     </div>
-
+        </main>
 </div>
