81109863e1d60d22087b45067a59d6aab38a42ce
Denason <mohammad.wartan@gmail.com>
2025-02-12 11:48:40 +0330

    category add mockup

--- resources/views/livewire/app/shop/product-list.blade.php
+++ resources/views/livewire/app/shop/product-list.blade.php
@@ -5,36 +5,36 @@
     @include('livewire.app.shop.category-inc.top-menu')
 
 
+    <main>
 
+        <div class="flex justify-center items-center m-2">
+            <h1 class="badge badge-primary badge-outline">خرید {{ $category->name }}</h1>
+        </div>
 
-
-        <main>
-    <section class="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-6 gap-4 mt-4">
-        @foreach($products as $product)
-            @livewire('app.component.product-card', ['product' => $product], key($product->id))
-        @endforeach
-    </section>
-
-
-
-
-
-
-        <section  class="mockup-browser border-base-300 border mt-8 mb-2">
-            <header class="mockup-browser-toolbar text-right">
-                <h1 class="justify-start text-right mx-auto">
-                    خرید    {{ $category->name }}
-                </h1>
-            </header>
-            <p class="border-base-300 flex text-justify justify-start border-t px-4 py-4 leading-8 text-gray-500 text-sm">
-                {{ $category->description }}
-            </p>
+        <section class="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-6 gap-4 mt-4">
+            @foreach($products as $product)
+                @livewire('app.component.product-card', ['product' => $product], key($product->id))
+            @endforeach
         </section>
 
 
-    <!-- Centered Pagination -->
-    <div class="mt-4 flex justify-center">
-        {{ $products->links() }}
-    </div>
-        </main>
+        <section class="mockup-browser border-base-300 border mt-8 mb-2">
+            <header class="mockup-browser-toolbar text-right">
+                <h2 class="justify-start text-right mx-auto">
+                    ویژگی‌های {{ $category->name }} که باید بدانید
+                </h2>
+            </header>
+            <article>
+                <p class="border-base-300 flex text-justify justify-start border-t px-4 py-4 leading-8 text-gray-500 text-sm">
+                    {{ $category->description }}
+                </p>
+            </article>
+        </section>
+
+
+        <!-- Centered Pagination -->
+        <div class="mt-4 flex justify-center">
+            {{ $products->links() }}
+        </div>
+    </main>
 </div>
