e3af8c8eb6b4cf8d1219cf0f34a5f4f7ec6944a0
Denason <mohammad.wartan@gmail.com>
2025-02-13 15:02:47 +0330

    last update npm

--- resources/views/livewire/app/shop/product-list.blade.php
+++ resources/views/livewire/app/shop/product-list.blade.php
@@ -4,16 +4,37 @@
     @endpush
     @include('livewire.app.shop.category-inc.top-menu')
 
-    <div class="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-6 gap-4 mt-4">
-        @foreach($products as $product)
-            @livewire('app.component.product-card', ['product' => $product], key($product->id))
-        @endforeach
-    </div>
+
+    <main>
+
+        <div class="flex justify-center items-center m-2">
+            <h1 class="badge badge-primary badge-outline">خرید {{ $category->name }}</h1>
+        </div>
+
+        <section class="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-6 gap-4 mt-4">
+            @foreach($products as $product)
+                @livewire('app.component.product-card', ['product' => $product], key($product->id))
+            @endforeach
+        </section>
 
 
-    <!-- Centered Pagination -->
-    <div class="mt-4 flex justify-center">
-        {{ $products->links() }}
-    </div>
+        <section class="mockup-browser border-base-300 border mt-8 mb-2">
+            <header class="mockup-browser-toolbar text-right">
+                <h2 class="justify-start text-right mx-auto">
+                    ویژگی‌های {{ $category->name }}
+                </h2>
+            </header>
+            <article>
+                <p class="border-base-300 flex text-justify justify-start border-t px-4 py-4 leading-8 text-gray-500 text-sm">
+                    {{ $category->description }}
+                </p>
+            </article>
+        </section>
 
+
+        <!-- Centered Pagination -->
+        <div class="mt-4 flex justify-center">
+            {{ $products->links() }}
+        </div>
+    </main>
 </div>
