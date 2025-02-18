7daec7c2470d4ff75deef7aed283d5a8dc147202
Denason <mohammad.wartan@gmail.com>
2025-02-18 11:52:27 +0330

    fix redirect master

--- resources/views/livewire/admin/template/multi-sidebar.blade.php
+++ resources/views/livewire/admin/template/multi-sidebar.blade.php
@@ -20,9 +20,10 @@
 
     <x-menu-sub title="بلاگ" icon="o-pencil-square">
 
-        <x-menu-item title="پیشخوان" icon="o-book-open" link="https://liftpal.ir/blogpress/wp-admin/index.php" external/>
-        <x-menu-item title="ایجاد" icon="o-pencil" link="https://liftpal.ir/blogpress/wp-admin/post-new.php" external/>
-        <x-menu-item title="لیست" icon="o-queue-list" link="https://liftpal.ir/blogpress/wp-admin/edit.php" external/>
+        <x-menu-item title="پیشخوان" icon="o-book-open" link="{{ env('APP_URL') }}/blogpress/wp-admin/index.php" external/>
+
+        <x-menu-item title="ایجاد" icon="o-pencil" link="{{ env('APP_URL') }}/blogpress/wp-admin/post-new.php" external/>
+        <x-menu-item title="لیست" icon="o-queue-list" link="{{ env('APP_URL') }}/blogpress/wp-admin/edit.php" external/>
 
 
 
