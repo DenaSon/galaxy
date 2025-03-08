b3005a6c42a3b92c06387253d116c3ac81f2a5a9
Denason <mohammad.wartan@gmail.com>
2025-03-09 00:57:58 +0330

    fix login input for number type

--- resources/views/livewire/app/component/address-modal.blade.php
+++ resources/views/livewire/app/component/address-modal.blade.php
@@ -46,7 +46,7 @@
         <x-select inline label="انتخاب شهر" icon="o-user" :options="$city_list" wire:model="city" class="mb-2"/>
 
 
-        <x-input locale="fa-IR" money type="number" inline label="کد پستی" clearable wire:model.live="postal_code"
+        <x-input type="number" inline label="کد پستی" clearable wire:model.live="postal_code"
                  class="mb-1"/>
 
         <x-textarea inline label="آدرس دقیق پستی" wire:model="address_line"/>
