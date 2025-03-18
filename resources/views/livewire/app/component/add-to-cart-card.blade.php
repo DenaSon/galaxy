e300809b2bbc544327ff7dc9d748e1eff3a2a186
Denason <mohammad.wartan@gmail.com>
2025-03-18 19:37:27 +0330

    fix addToCart-1-styles

--- resources/views/livewire/app/component/add-to-cart-card.blade.php
+++ resources/views/livewire/app/component/add-to-cart-card.blade.php
@@ -22,7 +22,7 @@
                         :options="$product->variants()->orderBy('price')->select(['id', 'type'])->get()"
                         option-value="id"
                         option-label="type"
-                        class="btn btn-sm  [&:checked]:!btn-success !text-white text-sm"
+                        class="btn btn-sm  [&:checked]:!btn-success [&:checked]:!text-white text-sm font-normal"
                         wire:model.live="variant"/>
 
                 </div>
