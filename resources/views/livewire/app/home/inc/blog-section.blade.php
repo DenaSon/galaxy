04cd1af3bca3ea01404b0ca7884a45dd39d23471
Denason <mohammad.wartan@gmail.com>
2025-03-22 11:48:32 +0330

    Add Wordpress Blogs to HomeIndex

--- resources/views/livewire/app/home/inc/blog-section.blade.php
+++ resources/views/livewire/app/home/inc/blog-section.blade.php
@@ -11,7 +11,7 @@
 
                 @foreach($blogs as $blog)
                     <div class="swiper-slide">
-                        @livewire('app.component.blog-card',['blog'=>$blog])
+                        @livewire('app.component.blog-card',['blog'=>$blog->id])
                     </div>
                 @endforeach
 
