de605e615b3a05a4ef38743fcf46211fe13c40fa
Denason <mohammad.wartan@gmail.com>
2025-03-22 11:17:23 +0330

    Add Wordpress Blogs to HomeIndex

--- config/wordpress.php
+++ config/wordpress.php
@@ -9,7 +9,7 @@ return
 
         //Database Info
         'wp_db_host' => env('WP_DB_HOST', 'localhost'),
-        'wp_db_user' => env('WP_DB_USER', 'root'),
+        'wp_db_user' => env('WP_DB_USERNAME', 'root'),
         'wp_db_password' => env('WP_DB_PASSWORD', ''),
         'wp_db_name' => env('WP_DB_NAME', 'blog'),
         'wp_db_prefix' => env('WP_DB_PREFIX', ''),
