9630eb42714a11ae067ed6ec8b5f646480191e0d
Denason <mohammad.wartan@gmail.com>
2025-03-22 11:44:47 +0330

    Add Wordpress Blogs to HomeIndex

--- resources/views/livewire/app/component/blog-card.blade.php
+++ resources/views/livewire/app/component/blog-card.blade.php
@@ -1,26 +1,31 @@
-<div class="w-full max-w-md mx-auto">
-    @foreach($blogs as $post)
-        <!-- Iterating over the posts -->
+<div class="w-full max-w-md mx-auto" wire:key="{{$blog['id']}}">
     <a @if(request()->routeIs('home.blog.indexBlog')) wire:navigate @endif
+
     class="block relative bg-white overflow-hidden rounded-lg shadow-lg border border-gray-200"
-       href="{{ route('singleBlog', ['id' => $post->id, 'title' => $post->slug]) }}">
+
+       href="{{ singleBlogUrl($blog['id'],$blog['title']['rendered']) }}">
         <div class="flex flex-col h-48 shadow-xl">
+
+
             <div class="relative h-40 overflow-hidden p-0">
-                @if($post->featuredImage)
-                    <!-- Checking if there's a featured image -->
-                    <img loading="lazy" src="{{ $post->featuredImage->url }}" alt="{{ $post->title }}"
+
+                @if(isset($blog['featured_image_url']))
+                    <img loading="lazy" src="{{ $blog['featured_image_url'] }}" alt="{{ $blog['title']['rendered'] }}"
                          class="w-full h-auto">
                 @endif
+
             </div>
+
 
             <div class="p-1">
                 <div class="text-center">
-                    <h2 class="mt-2 text-xs text-gray-700 leading-snug font-black">
-                        {{ $post->title }} <!-- Displaying post title -->
+                    <h2 class=" mt-2 text-xs text-gray-700 leading-snug font-black">
+
+                        {{ $blog->title }}
+
                     </h2>
                 </div>
             </div>
         </div>
     </a>
-    @endforeach
 </div>
