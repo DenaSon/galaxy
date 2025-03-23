e214fceb0ff9aee489929ab548087437d7b6da0b
Denason <mohammad.wartan@gmail.com>
2025-03-23 12:15:40 +0330

    rollback

--- resources/views/livewire/app/component/blog-card.blade.php
+++ resources/views/livewire/app/component/blog-card.blade.php
@@ -1,29 +1,29 @@
-<div class="w-full max-w-md mx-auto" wire:key="{{$blog['id']}}">
-    <a @if(request()->routeIs('home.blog.indexBlog')) wire:navigate @endif
+{{--<div class="w-full max-w-md mx-auto" wire:key="{{$blog['id']}}">--}}
+{{--    <a @if(request()->routeIs('home.blog.indexBlog')) wire:navigate @endif--}}
 
-        class="block relative bg-white overflow-hidden rounded-lg shadow-lg border border-gray-200"
+{{--        class="block relative bg-white overflow-hidden rounded-lg shadow-lg border border-gray-200"--}}
 
-        href="{{ singleBlogUrl($blog['id'],$blog['title']['rendered']) }}">
-        <div class="flex flex-col h-48 shadow-xl">
+{{--        href="{{ singleBlogUrl($blog['id'],$blog['title']['rendered']) }}">--}}
+{{--        <div class="flex flex-col h-48 shadow-xl">--}}
 
 
-            <div class="relative h-40 overflow-hidden p-0">
+{{--            <div class="relative h-40 overflow-hidden p-0">--}}
 
-                @if(isset($blog['featured_image_url']))
-                    <img loading="lazy" src="{{ $blog['featured_image_url'] }}" alt="{{ $blog['title']['rendered'] }}"
-                         class="w-full h-auto">
-                @endif
+{{--                @if(isset($blog['featured_image_url']))--}}
+{{--                    <img loading="lazy" src="{{ $blog['featured_image_url'] }}" alt="{{ $blog['title']['rendered'] }}"--}}
+{{--                         class="w-full h-auto">--}}
+{{--                @endif--}}
 
-            </div>
-            <div class="p-1">
-                <div class="text-center">
-                    <h2 class=" mt-2 text-xs text-gray-700 leading-snug font-black">
-                        {{ $blog['title']['rendered'] }}
+{{--            </div>--}}
+{{--            <div class="p-1">--}}
+{{--                <div class="text-center">--}}
+{{--                    <h2 class=" mt-2 text-xs text-gray-700 leading-snug font-black">--}}
+{{--                        {{ $blog['title']['rendered'] }}--}}
 
-                    </h2>
-                </div>
-            </div>
-        </div>
-    </a>
-</div>
+{{--                    </h2>--}}
+{{--                </div>--}}
+{{--            </div>--}}
+{{--        </div>--}}
+{{--    </a>--}}
+{{--</div>--}}
 
