7f9f0762d61395ef92e58b709e3a70f652c2671c
Denason <mohammad.wartan@gmail.com>
2025-04-14 12:31:36 +0330

    add slug

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -1,8 +1,11 @@
 <?php
 
+use Denason\PersianSlug\SlugGeneratorInterface;
+
 return [
     App\Providers\AppServiceProvider::class,
     App\Providers\DatabaseServiceProvider::class,
     App\Providers\GateServiceProvider::class,
     App\Providers\VoltServiceProvider::class,
+    SlugGeneratorInterface::class,
 ];
