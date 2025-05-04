314fad56105d680162ea7a97418007f942097777
Mohammad Assadi <denasoft@live.com>
2025-05-04 13:49:52 +0330

    FIX CONFIG KEYS

--- config/sms.php
+++ config/sms.php
@@ -7,8 +7,8 @@ return [
     'success_order_sms_code' => env('SUCCESS_SMS_CODE'),
     'master_notify_payment_sms' => env('MASTER_NOTIFY_SMS'),
 
-    'manager_request_notify' => env('manager_request_notify_id'),
-    'technical_report_notify' => env('technician_report_notify'),
+    'manager_request_notify' => env('MANAGER_REQUEST_NOTIFY'),
+    'technical_report_notify' => env('TECHNICIAN_REPORT_NOTIFY'),
 
 
 ];
