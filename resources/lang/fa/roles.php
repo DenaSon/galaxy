44e11ba5fc96467fd4dd2936d81d8a6ed63c6cde
Mohammad Assadi <denasoft@live.com>
2025-05-04 19:32:39 +0330

    Update UserList-update-2-persian added

---
+++ resources/lang/fa/roles.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'master' => 'مدیر',
+    'customer' => 'مشتری',
+    'admin' => 'مدیر کل',
+    'technician' => 'تکنسین',
+    'manager' => 'مدیر ساختمان',
+    'builder' => 'مدیر ساختمان',
+
+];
+
--- resources/views/livewire/admin/component/user-list.blade.php
+++ resources/views/livewire/admin/component/user-list.blade.php
@@ -45,7 +45,8 @@
                         <td class="text-xs" title="">
 
                             @foreach($user->roles()->get() as $role)
-                                {{ $role->name }}
+                                {{ $user->roles->map(fn($role) => __('roles.' . $role->name))->join(', ') }}
+
                             @endforeach
 
 
